98e04e235f8dbac5d7ffa5f8fea58b9f52328431
Shan Oovattil <shan.oovattil@gmail.com>
2023-10-15 19:02:09 +0530

    Encapsulation Demo
    This is to implement OOPS Encapsulation

---
+++ src/Car.java
@@ -0,0 +1,14 @@
+public class Car
+{
+    private String name;
+
+    public String getName()
+    {
+        return  name;
+    }
+
+    public void setName(String sName)
+    {
+        this.name = sName;
+    }
+}
---
+++ src/EncampsDemo.java
@@ -0,0 +1,9 @@
+public class EncampsDemo
+{
+    public static void main(String[] args)
+    {
+        Car car = new Car();
+        car.setName("MG Hector");
+        System.out.println(car.getName());
+    }
+}
